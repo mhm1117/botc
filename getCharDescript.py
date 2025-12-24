e01aed78f2b1d2a57d0c90ab47d100bd4b842be6
mhmichel <mhmichel17@gmail.com>
2025-12-23 22:37:06 -0800

    got character names and descriptions

--- getCharDescript.py
+++
@@ -1,8 +0,0 @@
-file_path = 'characters.txt'
-
-try:
-    with open(file_path, 'r') as file:
-        content = file.read()
-        print(content)
-except FileNotFoundError:
-    print(f"Error: The file '{file_path}' was not found.")
